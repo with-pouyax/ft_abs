ce22bfbdc95b46c55323599197391f4599c283ab
Pouya Ghajardavalou <ghajar.pouya@gmail.com>
2024-03-22 07:44:32 -0700

    file added

---
+++ ft_abs.h
@@ -0,0 +1,14 @@
+#ifndef FT_ABS_H
+# define FT_ABS_H
+
+static inline int	ft_abs(int value)
+{
+	if (value < 0)
+		return (-value);
+	else
+		return (value);
+}
+
+# define ABS(Value) ft_abs(Value)
+
+#endif
